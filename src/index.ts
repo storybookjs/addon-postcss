f72f5a17c52d2b7a5cd4e9c68ae66b565328ef40
Blaine Bublitz <blaine.bublitz@gmail.com>
2021-01-20 13:22:18 -0700

    feat: initial implementation of addon

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import { Configuration } from 'webpack';
+import { logger } from '@storybook/node-logger';
+import postcss from 'postcss';
+
+type PostcssFactory = (
+  plugins?: postcss.AcceptedPlugin[] | undefined,
+) => postcss.Processor;
+
+// TODO(blaine): Should we take PostCSS config in plugin options?
+export const webpack = (
+  webpackConfig: Configuration = {},
+  options: { postcss?: PostcssFactory },
+): Configuration => {
+  const postcssFactory = options.postcss ?? postcss;
+  const { version } = postcssFactory();
+
+  logger.info(`=> Using PostCSS preset with version ${version}`);
+
+  return {
+    ...webpackConfig,
+    module: {
+      ...webpackConfig.module,
+      rules: [
+        ...(webpackConfig.module?.rules ?? []),
+        {
+          test: /\.css$/,
+          sideEffects: true,
+          use: [
+            require.resolve('style-loader'),
+            {
+              loader: require.resolve('css-loader'),
+              options: {
+                importLoaders: 1,
+              },
+            },
+            {
+              loader: require.resolve('postcss-loader'),
+              options: {
+                implementation: postcssFactory,
+              },
+            },
+          ],
+        },
+      ],
+    },
+  };
+};
